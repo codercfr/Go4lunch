7bcc1177e9f25841a30ba109016c6129c2cf7bc5
cassore.c@gmail.Com <cassore.c@gmail.Com>
2022-04-14 15:03:27 +0200

    14.04.2022 notif ok / afficher tous les utilisateurs ok/ régler visuel en attente

--- app/src/main/java/com/example/go4lunch/NotificationCoworker.java
+++ app/src/main/java/com/example/go4lunch/NotificationCoworker.java
@@ -17,16 +17,21 @@ import androidx.work.WorkerParameters;
 
 import com.example.go4lunch.model.Users;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.events.EventHandler;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -36,8 +41,12 @@ public class NotificationCoworker extends Worker  {
     public static final String CHANNEL_1_ID = "channel1";
     private FirebaseDatabase mDatabase;
     private DatabaseReference databaseReference;
-    private Users user;
+    private Users user,userTest;
     private FirebaseAuth mAuth;
+    private List<Users> usersList= new ArrayList<>();
+    private List<Users> userFirebase= new ArrayList<>();
+    private List<String>userName= new ArrayList<>();
+
 
 
 
@@ -57,19 +66,50 @@ public class NotificationCoworker extends Worker  {
         databaseReference.child((mAuth.getCurrentUser()).getUid()).get().addOnSuccessListener(dataSnapshot ->  {
             user= dataSnapshot.getValue(Users.class);
         });
+        try {
+            ValueEventListener eventListener = new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
 
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        user = ds.getValue(Users.class);
+                        usersList.add(user);
+                    }
+                    userFirebase.addAll(usersList);
+                }
+                @Override
+                public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                }
+            };
+            databaseReference.addListenerForSingleValueEvent(eventListener);
+            usersList.size();
+        }catch (Exception e) {
+            e.printStackTrace();
+            usersList.size();
+        }
         // afficher la notif ici.
         //vérifier si la personne veut afficher les notfications ou non.
         // pour crée un notif channel on a besoin de faire extends Application.
         //Il rappellera à l'utilisateur le nom du restaurant
         //qu'il a choisi, l'adresse, ainsi que la liste des collègues qui viendront avec lui.
 
-        notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        //Peux permettre de retrouver directement
+        //databaseReference.child("users").orderByChild("userName").equalTo("JohnDoe")
 
+        for(int i = 0; i< usersList.size();i++){
+            if(usersList.get(i).getRestaurantName().equals(user.getRestaurantName())) {
+                userName.add(usersList.get(i).getUsername());
+            }
+            userTest=usersList.get(i);
+        }
+
+
+        notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        // Mettre une condition pour pouvoir avoir que la liste des collégues qui ont le meme restaurant Name
         Notification notification = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.restaurant_icon)
                 .setContentTitle(getApplicationContext().getString(R.string.notifcation))
-                .setContentText(user.getRestaurantName())
+                .setContentText(user.getUsername()+" "+user.getRestaurantName()+" autres collègues :"+userName.toString())
                 //.setContentText("Vous allez au"+user.getRestaurantName()+""+user.getAdresseRestaurant())
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .build();
